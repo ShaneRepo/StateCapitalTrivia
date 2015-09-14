6cd455d808b667ac147971471a1f53f63da6b454
Shane <shane8641@live.com>
2015-09-14 18:25:06 -0400

    added comments to my prototype

--- StateCapitalTrivia/Form1.cs
+++ StateCapitalTrivia/Form1.cs
@@ -21,28 +21,28 @@ namespace StateCapitalTrivia
         private void Form1_Load(object sender, EventArgs e)
         {
             try
-            {
-                string text;
-                StreamReader inputfile;
-                //inputfile = File.OpenText("stateCapitals.txt");
+            {     
+                // read all lines from file and store them in an array
                 string[] lines = System.IO.File.ReadAllLines("stateCapitals.txt");
+                // instantiate an array for each question
                 string[] questionOne = new string[6];
                 string[] questionTwo = new string[6];
                 string[] questionThree = new string[6];
                 string[] questionFour = new string[6];
+                // copy each question into separate arrays
                 Array.Copy(lines, 0, questionOne, 0, 6);
                 Array.Copy(lines, 6, questionTwo, 0, 6);
                 Array.Copy(lines, 12, questionThree, 0, 6);
                 Array.Copy(lines, 18, questionFour, 0, 6);
 
+                // testing array copy works as intended for my purposes
+                //foreach (string line in questionFour)
+                //{
 
-                foreach (string line in questionFour)
-                {
+                //    textBox1.Text += line;
+                //    textBox1.Text += "\r\n";
 
-                    textBox1.Text += line;
-                    textBox1.Text += "\r\n";
-
-                }
+                //}
                 //while (!inputfile.EndOfStream)
                 //{
                 //    text = inputfile.ReadLine();
